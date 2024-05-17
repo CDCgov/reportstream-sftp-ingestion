edc6ef075978f15d097222bea54c85104c3bb746
halprin <halprin@users.noreply.github.com>
2024-05-17 13:46:49 -0600

    Add staging environment

--- operations/environments/stg/main.tf
+++ operations/environments/stg/main.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.102.0"
+      version = "3.103.1"
     }
   }
 
   # Use a remote Terraform state in Azure Storage
   backend "azurerm" {
-    resource_group_name  = "csels-rsti-dev-moderate-rg"
-    storage_account_name = "cdcintermediaryterraform"
+    resource_group_name  = "csels-rsti-stg-moderate-rg"
+    storage_account_name = "cdcintermediarytrfrmstg"
     container_name       = "terraform-state"
-    key                  = "rs-sftp-dev.terraform.tfstate"
+    key                  = "rs-sftp-staging.terraform.tfstate"
   }
 }
 
@@ -27,6 +27,6 @@ provider "azurerm" {
 module "template" {
   source = "../../template/"
 
-  environment          = "dev"
+  environment          = "stg"
   deployer_id          = "f5feabe7-5d37-40ba-94f2-e5c0760b4561"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 //github app registration in CDC Azure Entra
 }
