f34993a856e0aded7a8a67d5d1a5321869db2b7c
halprin <halprin@users.noreply.github.com>
2024-05-22 11:28:34 -0600

    Get the local file sender work when running locally through docker compose

--- src/local/sender.go
+++ src/local/sender.go
@@ -11,16 +11,17 @@ type FileSender struct {
 }
 
 func (receiver FileSender) SendMessage(message []byte) (string, error) {
-	//dir := "localdata"
-	//err := os.MkdirAll(dir, 0755)
-	//if err != nil {
-	//	return "", err
-	//}
+	folder := "localdata"
+
+	err := os.MkdirAll(folder, 0755)
+	if err != nil {
+		return "", err
+	}
 
 	randomUuid := uuid.NewString()
 
-	filePath := filepath.Join("localdata", fmt.Sprintf("%s.txt", randomUuid))
-	err := os.WriteFile(filePath, message, 0644) // permissions = owner read/write, group read, other read
+	filePath := filepath.Join(folder, fmt.Sprintf("%s.txt", randomUuid))
+	err = os.WriteFile(filePath, message, 0644) // permissions = owner read/write, group read, other read
 	if err != nil {
 		return "", err
 	}
