ec9549cc825a157d18ba6685125e11dd0f636a30
Sylvie <sschuresko@flexion.us>
2024-05-23 14:43:42 -0500

    Add GoDocs to interfaces
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>

--- src/cmd/blob.go
+++ src/cmd/blob.go
@@ -1,5 +1,7 @@
 package main
 
+// The BlobHandler interface is about interacting with file data,
+// e.g. in Azure Blob Storage or a local filesystem
 type BlobHandler interface {
 	FetchFile(blobPath string) ([]byte, error)
 }
