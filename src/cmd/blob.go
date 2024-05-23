0a7be4217e437a58f5e35afa38d2ffeb70319e35
Sylvie <sschuresko@flexion.us>
2024-05-23 14:51:45 -0500

    Use an interface for Azure blob storage (#35)
    * use interfaces, rename some stuff
    
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>
    
    * Add GoDocs to interfaces
    
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: jcrichlake <145698165+jcrichlake@users.noreply.github.com>

---
+++ src/cmd/blob.go
@@ -0,0 +1,14 @@
+package main
+
+// The BlobHandler interface is about interacting with file data,
+// e.g. in Azure Blob Storage or a local filesystem
+type BlobHandler interface {
+	FetchFile(blobPath string) ([]byte, error)
+}
+
+/**
+Future things to implement:
+put file
+move file
+delete file
+*/
--- src/cmd/sender.go
+++ src/cmd/sender.go
@@ -1,5 +1,7 @@
 package main
 
+// The MessageSender interface is about delivering data to external services.
+// Currently, we send messages to ReportStream or to a mock service for testing.
 type MessageSender interface {
 	SendMessage(message []byte) (string, error)
 }
