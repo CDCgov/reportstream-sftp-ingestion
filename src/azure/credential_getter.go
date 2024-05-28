c3216c46d2bdc9a2e88b59a4b6fddcc9f0cc3603
Sylvie <sschuresko@flexion.us>
2024-05-28 15:39:45 -0500

    let's try getting secrets from Azure

--- src/azure/credential_getter.go
+++ src/azure/credential_getter.go
@@ -1,25 +1,51 @@
 package azure
 
 import (
+	"context"
 	"crypto/rsa"
-	"fmt"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets"
 	"github.com/golang-jwt/jwt/v5"
+	"log/slog"
 	"os"
-	"path/filepath"
 )
 
 type CredentialGetter struct{}
 
 func (credentialGetter CredentialGetter) GetPrivateKey(privateKeyName string) (*rsa.PrivateKey, error) {
-	//TODO - Put the real code here
+	vaultURI := os.Getenv("AZURE_KEY_VAULT_URI")
+	slog.Info("got key info", slog.String("key name", privateKeyName), slog.String("vaultURI", vaultURI))
 
-	pem, err := os.ReadFile(filepath.Join("mock_credentials", fmt.Sprintf("%s.pem", privateKeyName)))
+	// Create a credential using the NewDefaultAzureCredential type.
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		slog.Error("failed to obtain a credential: ", slog.Any("error", err))
+		return nil, err
+	}
+
+	// Establish a connection to the Key Vault client
+	client, err := azsecrets.NewClient(vaultURI, cred, nil)
+	if err != nil {
+		slog.Error("failed to create a client: ", slog.Any("error", err))
+		return nil, err
+	}
+	slog.Info("created client")
+
+	version := ""
+	resp, err := client.GetSecret(context.TODO(), privateKeyName, version, nil)
+	if err != nil {
+		slog.Error("failed to get the secret ", slog.Any("error", err))
+		return nil, err
+	}
+
+	slog.Info("got secret")
+
+	pem := *resp.Value
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(pem))
 	if err != nil {
 		return nil, err
 	}
-	key, err := jwt.ParseRSAPrivateKeyFromPEM(pem)
-	if err != nil {
-		return nil, err
-	}
+	slog.Info("parsed pem to key")
 	return key, nil
 }
