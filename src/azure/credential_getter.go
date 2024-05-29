df5e04b70dc0cf0fe7972e3fb0df0edfa5a44ab6
Sylvie <sschuresko@flexion.us>
2024-05-29 12:00:05 -0500

    try other keyvault package
    Co-Authored-By: halprin <halprin@users.noreply.github.com>

--- src/azure/credential_getter.go
+++ src/azure/credential_getter.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"crypto/rsa"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets"
+	"github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/azsecrets"
 	"github.com/golang-jwt/jwt/v5"
 	"log/slog"
 	"os"
@@ -17,7 +17,8 @@ func (credentialGetter CredentialGetter) GetPrivateKey(privateKeyName string) (*
 	slog.Info("got key info", slog.String("key name", privateKeyName), slog.String("vaultURI", vaultURI))
 
 	// Create a credential using the NewDefaultAzureCredential type.
-	cred, err := azidentity.NewManagedIdentityCredential(nil)
+	//cred, err := azidentity.NewManagedIdentityCredential(nil)
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		slog.Error("failed to obtain a credential: ", slog.Any("error", err))
 		return nil, err
