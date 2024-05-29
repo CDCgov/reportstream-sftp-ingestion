ac91f6f1496783d763621a155c8df05c595298fa
halprin <halprin@users.noreply.github.com>
2024-05-29 11:15:25 -0600

    Go back, again, to NewManagedIdentityCredential

--- src/azure/credential_getter.go
+++ src/azure/credential_getter.go
@@ -17,8 +17,8 @@ func (credentialGetter CredentialGetter) GetPrivateKey(privateKeyName string) (*
 	slog.Info("got key info", slog.String("key name", privateKeyName), slog.String("vaultURI", vaultURI))
 
 	// Create a credential using the NewDefaultAzureCredential type.
-	//cred, err := azidentity.NewManagedIdentityCredential(nil)
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	cred, err := azidentity.NewManagedIdentityCredential(nil)
+	//cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		slog.Error("failed to obtain a credential: ", slog.Any("error", err))
 		return nil, err
