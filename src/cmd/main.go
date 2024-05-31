5dc24f8d6b3eed3a04a2bcc272104d6a0536dd5b
halprin <halprin@users.noreply.github.com>
2024-05-30 20:46:19 -0600

    Create a usecase so not everything is in the main method

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"github.com/CDCgov/reportstream-sftp-ingestion/azure"
-	"github.com/CDCgov/reportstream-sftp-ingestion/local"
-	"github.com/CDCgov/reportstream-sftp-ingestion/report_stream"
 	"io"
 	"log/slog"
 	"net/http"
@@ -19,41 +16,16 @@ func main() {
 
 	go setupHealthCheck()
 
-	azureBlobConnectionString := os.Getenv("AZURE_BLOB_CONNECTION_STRING")
-	blobHandler, err := azure.NewStorageHandler(azureBlobConnectionString)
+	usecase, err := NewReadAndSendUsecase()
 	if err != nil {
-		slog.Error("Failed to init Azure blob client", slog.Any("error", err))
-		os.Exit(1)
+		slog.Warn("Failed to init the usecase", slog.Any("error", err))
+		slog.Info("Continuing for now while debugging")
 	}
 
-	filepath := "order_message.hl7"
-	content, err := readAzureFile(blobHandler, filepath)
+	err = usecase.ReadAndSend()
 	if err != nil {
-		slog.Error("Failed to read the file", slog.String("filepath", filepath), slog.Any("error", err))
-		os.Exit(1)
-	}
-
-	reportStreamBaseUrl := os.Getenv("REPORT_STREAM_URL_PREFIX")
-	var messageSender MessageSender
-
-	if reportStreamBaseUrl == "" {
-		slog.Info("No report stream url prefix set, using local sender instead")
-		messageSender = local.FileSender{}
-	} else {
-		slog.Info("Found report stream url prefix, will send to ReportStream")
-		messageSender, err = report_stream.NewSender()
-		if err != nil {
-			slog.Warn("Failed to construct the ReportStream sender", slog.Any("error", err))
-			slog.Info("actually continuing just for now while we debug")
-		}
-	}
-
-	reportId, err := messageSender.SendMessage(content)
-	if err != nil {
-		slog.Warn("Failed to send the file to ReportStream", slog.Any("error", err))
-		slog.Info("actually continuing just for now while we debug")
-	} else {
-		slog.Info("File sent to ReportStream", slog.String("reportId", reportId))
+		slog.Warn("Usecase failed", slog.Any("error", err))
+		slog.Info("Continuing for now while debugging")
 	}
 
 	for {
@@ -92,16 +64,4 @@ func setupHealthCheck() {
 	if err != nil {
 		slog.Error("Failed to start health check", slog.Any("error", err))
 	}
-}
-
-func readAzureFile(blobHandler BlobHandler, filePath string) ([]byte, error) {
-	content, err := blobHandler.FetchFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	//TODO: Auth and call ReportStream
-	slog.Info(string(content))
-
-	return content, nil
 }
---
+++ src/cmd/read_and_send_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func Test_ReadAndSendUsecase_failsToReadBlob(t *testing.T) {
+	mockBlobHandler := &MockBlobHandler{}
+	mockBlobHandler.On("FetchFile", "order_message.hl7").Return([]byte{}, errors.New("it blew up"))
+
+	usecase := ReadAndSendUsecase{blobHandler: mockBlobHandler}
+
+	err := usecase.ReadAndSend()
+
+	assert.Error(t, err)
+}
+
+func Test_ReadAndSendUsecase_failsToSendMessage(t *testing.T) {
+	mockBlobHandler := &MockBlobHandler{}
+	mockBlobHandler.On("FetchFile", "order_message.hl7").Return([]byte("The DogCow went Moof!"), nil)
+
+	mockMessageSender := &MockMessageSender{}
+	mockMessageSender.On("SendMessage", mock.Anything).Return("", errors.New("sending message failed"))
+
+	usecase := ReadAndSendUsecase{blobHandler: mockBlobHandler, messageSender: mockMessageSender}
+
+	err := usecase.ReadAndSend()
+
+	assert.Error(t, err)
+}
+
+func Test_ReadAndSendUsecase_successfulReadAndSend(t *testing.T) {
+	mockBlobHandler := &MockBlobHandler{}
+	mockBlobHandler.On("FetchFile", "order_message.hl7").Return([]byte("The DogCow went Moof!"), nil)
+
+	mockMessageSender := &MockMessageSender{}
+	mockMessageSender.On("SendMessage", mock.Anything).Return("epic report ID", nil)
+
+	usecase := ReadAndSendUsecase{blobHandler: mockBlobHandler, messageSender: mockMessageSender}
+
+	err := usecase.ReadAndSend()
+
+	assert.NoError(t, err)
+}
+
+type MockBlobHandler struct {
+	mock.Mock
+}
+
+func (receiver *MockBlobHandler) FetchFile(blobPath string) ([]byte, error) {
+	args := receiver.Called(blobPath)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+type MockMessageSender struct {
+	mock.Mock
+}
+
+func (receiver *MockMessageSender) SendMessage(message []byte) (string, error) {
+	args := receiver.Called(message)
+	return args.Get(0).(string), args.Error(1)
+}
