5002afea9c14efef3388212f5641129b710dfd40
Sylvie <sschuresko@flexion.us>
2024-06-07 11:02:49 -0500

    use the name when it should be a name
    Co-Authored-By: pluckyswan <96704946+pluckyswan@users.noreply.github.com>

--- operations/template/queue.tf
+++ operations/template/queue.tf
@@ -1,9 +1,9 @@
 resource "azurerm_storage_queue" "message_queue" {
   name                 = "blob-message-queue"
-  storage_account_name = azurerm_storage_account.storage
+  storage_account_name = azurerm_storage_account.storage.name
 }
 
 resource "azurerm_storage_queue" "message_dead_letter_queue" {
   name                 = "blob-message-dead-letter-queue"
-  storage_account_name = azurerm_storage_account.storage
+  storage_account_name = azurerm_storage_account.storage.name
 }
