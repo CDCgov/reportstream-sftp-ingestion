0fdb0f0482ff60820476d35860ac57b7d3243bf1
halprin <halprin@users.noreply.github.com>
2024-06-10 10:45:47 -0600

    Add user facing logging for whether code coverage is high enough

--- codeCoverageThresholdCheck.sh
+++ codeCoverageThresholdCheck.sh
@@ -9,7 +9,9 @@ COVERAGE=$(go tool cover -func ./coverage.out | grep total | grep -Eo '[0-9]+\.[
 echo "Code coverage is $COVERAGE%, and the threshold is $THRESHOLD%"
 
 if (( $(echo "$COVERAGE $THRESHOLD" | awk '{print ($1 < $2)}') )); then
+  echo "Code coverage is not high enough"
   exit 1
 fi
 
+echo "Code coverage passes the threshold"
 exit 0
