6f6d8aac0117ae2e7a991047f28e567b7f7240d7
halprin <halprin@users.noreply.github.com>
2024-06-10 09:09:35 -0600

    Institute a code coverage threshold check

---
+++ codeCoverageThresholdCheck.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+pushd ./src/ || exit
+
+THRESHOLD=15
+COVERAGE=$(go tool cover -func ./coverage.out | grep total | grep -Eo '[0-9]+\.[0-9]+')
+
+echo "Code coverage is $COVERAGE, and the threshold is $THRESHOLD"
+
+if (( $(echo "$COVERAGE $THRESHOLD" | awk '{print ($1 < $2)}') )); then
+  exit 1
+fi
+
+exit 0
