9bed30659d9100eeeede340dad650e0281ba7158
Sylvie <sschuresko@flexion.us>
2024-06-12 16:27:42 -0500

    add clarifying comment, notes dump
    Co-Authored-By: pluckyswan <96704946+pluckyswan@users.noreply.github.com>

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -23,6 +23,7 @@ func NewStorageHandler(conn string) (StorageHandler, error) {
 	return StorageHandler{blobClient: blobClient}, nil
 }
 
+// FetchFile retrieves the specified blob from Azure. The `blobPath` is everything after the container in the URL
 func (receiver StorageHandler) FetchFile(blobPath string) ([]byte, error) {
 	// The container name for CA will be added as part of card 1077 and will be configurable in 1081
 	containerName := "sftp"
@@ -40,6 +41,7 @@ func (receiver StorageHandler) FetchFile(blobPath string) ([]byte, error) {
 
 func (receiver StorageHandler) MoveFile(sourceBlobPath string, destinationBlobPath string) error {
 
+	// Borrowed from https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/storage/azblob/blob/examples_test.go
 	accountName, accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME"), os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
 
 	// Create a containerClient object to a container where we'll create a blob and its snapshot.
@@ -55,4 +57,18 @@ func (receiver StorageHandler) MoveFile(sourceBlobPath string, destinationBlobPa
 	src := "https://cdn2.auth0.com/docs/media/addons/azure_blob.svg"
 	startCopy, err := blobClient.StartCopyFromURL(context.TODO(), src, nil)
 
+	/* Notes on above:
+	- it feels icky to be creating a new client for each `move` action
+	- but it probably feels even worse to download a file just to upload it again
+	- not totally sure if `CopyFromUrl` would work for us - don't know if it needs to be a public URL?
+		Might just need to update TF permissions
+	- we can't use the `blob` client created in this method as a general client because it is blob-specific, so we
+		can't just swap this new buddy into NewStorageHandler
+	- we don't currently have env vars for account name and key. Could use `blob.NewClientFromConnectionString`, but
+		that will also need a blob URL
+	- if we have to use blob URLs (rather than paths), we're going to have to either build them from pieces, or get the
+		URL out of the file create event (which means going back to parsing the `data` object)
+
+	*/
+	return nil
 }
