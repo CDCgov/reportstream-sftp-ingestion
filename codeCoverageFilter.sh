1f0e341b2e6b576a871cf21dc5129b8c9efb47e6
jcrichlake <jeff.crichlake@agile6.com>
2024-06-19 14:28:54 -0400

    Adding method to exclude code coverage

---
+++ codeCoverageFilter.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+pushd ./src/ || exit
+
+EXCLUDE_FILES=(
+  github.com/CDCgov/reportstream-sftp-ingestion/cmd/main.go
+  github.com/CDCgov/reportstream-sftp-ingestion/storage/azure.go
+)
+
+for exclusion in "${EXCLUDE_FILES[@]}"
+do
+  sed -i '' "/${exclusion//\//\\/}/d" ./coverage.out
+done
