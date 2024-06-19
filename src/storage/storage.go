3261f17d921ebd5a7fd39899629fac00022c932a
halprin <halprin@users.noreply.github.com>
2024-06-19 08:49:22 -0600

    Remove commented out code.
    Co-authored-by: jcrichlake <jcrichlake@users.noreply.github.com>

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -78,44 +78,6 @@ func (receiver StorageHandler) MoveFile(sourceUrl string, destinationUrl string)
 
 	slog.Info("Successfully uploaded file", slog.String("destinationUrl", destinationUrl), slog.Any("uploadResponse", uploadResponse))
 
-	//// the storage account-level azblob client used on the StorageHandler struct doesn't have a `copy` function,
-	//// so we have to use a blob-file-specific client for copying
-	//connectionString := os.Getenv("AZURE_STORAGE_CONNECTION_STRING")
-	//blobClient, err := blob.NewClientFromConnectionString(connectionString, containerName, destinationUrlParts.BlobName, nil)
-	//if err != nil {
-	//	slog.Error("Error creating blob client")
-	//	return err
-	//}
-	//copyResponse, err := blobClient.CopyFromURL(context.TODO(), sourceUrl, nil)
-	//if err != nil {
-	//	slog.Error("Error copying file", slog.String("sourceUrl", sourceUrl), slog.String("destinationUrl", destinationUrl), slog.Any("error", err))
-	//	return err
-	//}
-
-	//startCopy, err := blobClient.StartCopyFromURL(context.TODO(), sourceUrl, nil)
-	//if err != nil {
-	//	slog.Error("Error starting blob copy")
-	//	return err
-	//}
-	//
-	//copyStatus := *startCopy.CopyStatus
-	//slog.Info("copy status before copy", slog.Any("copyStatus", copyStatus))
-	//for copyStatus == blob.CopyStatusTypePending {
-	//	time.Sleep(time.Second * 2)
-	//	getMetadata, err := blobClient.GetProperties(context.TODO(), nil)
-	//	if err != nil {
-	//		slog.Error("Error during blob copy")
-	//		return err
-	//	}
-	//	copyStatus = *getMetadata.CopyStatus
-	//	slog.Info("copy status during copy", slog.Any("copyStatus", copyStatus))
-	//	slog.Info("metadata during copy", slog.Any("getMetadata", getMetadata))
-	//}
-	//slog.Info("copy status after copy", slog.Any("copyStatus", copyStatus))
-	//slog.Info("Copied blob", slog.String("source URL", sourceUrl), slog.String("destination URL", destinationUrl), slog.Any("copyReponse", copyResponse))
-	//getMetadata, err := blobClient.GetProperties(context.TODO(), nil)
-	//slog.Info("metadata after copy", slog.Any("getMetadata", getMetadata))
-
 	_, err = receiver.blobClient.DeleteBlob(context.Background(), containerName, sourceUrlParts.BlobName, &azblob.DeleteBlobOptions{})
 	if err != nil {
 		slog.Error("Error deleting source file after copy", slog.String("source URL", sourceUrl))
