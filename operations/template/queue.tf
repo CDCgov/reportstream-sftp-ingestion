863dcd0d387337dc127808c1ccffab9f64950998
Sylvie <sschuresko@flexion.us>
2024-06-20 14:18:01 -0500

    fix the copy pasta
    Co-Authored-By: halprin <halprin@users.noreply.github.com>
    Co-Authored-By: pluckyswan <96704946+pluckyswan@users.noreply.github.com>
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>

--- operations/template/queue.tf
+++ operations/template/queue.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_queue" "message_queue" {
   storage_account_name = azurerm_storage_account.storage.name
 }
 
-resource "azurerm_storage_queue" "message_queue" {
+resource "azurerm_storage_queue" "dead_letter_queue" {
   name                 = "blob-message-dead-letter-queue"
   storage_account_name = azurerm_storage_account.storage.name
 }
