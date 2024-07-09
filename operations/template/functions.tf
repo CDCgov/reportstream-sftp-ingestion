c9b76c49e4a10b7879fc81a6cfb7842f61d7c4ad
James Herr <jherr@flexion.us>
2024-07-09 15:10:24 -0500

    Re-configure polling function app function
    Co-authored-by: saquino0827 <saquino@flexion.us>
    Co-authored-by: Sylvie <sschuresko@flexion.us>
    Co-authored-by: pluckyswan <96704946+pluckyswan@users.noreply.github.com>

--- operations/template/functions.tf
+++ operations/template/functions.tf
@@ -1,19 +1,11 @@
 # resource for Azure Functions for SFTP
-resource "azurerm_linux_function_app" "polling_trigger_function" {
+resource "azurerm_linux_function_app" "polling_trigger_function_app" {
   name                       = "polling-function-${var.environment}"
   location                   = data.azurerm_resource_group.group.location
   resource_group_name        = data.azurerm_resource_group.group.name
   service_plan_id            = azurerm_service_plan.plan.id
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-
-  site_config {}
-}
-
-resource "azurerm_linux_function_app_slot" "polling_trigger_function_slot" {
-  name                 = "polling-function-slot-${var.environment}"
-  function_app_id      = azurerm_linux_function_app.polling_trigger_function.id
-  storage_account_name = azurerm_storage_account.storage.name
 
   site_config {
     app_scale_limit = 1
@@ -26,4 +18,17 @@ resource "azurerm_linux_function_app_slot" "polling_trigger_function_slot" {
       node_version = "20"
     }
   }
+}
+
+resource "azurerm_function_app_function" "polling_trigger_function_app_function" {
+  name            = "polling-function-app-function-${var.environment}"
+  function_app_id = azurerm_linux_function_app.polling_trigger_function_app.id
+  language        = "TypeScript"
+
+  file {
+    name    = "caDphTimerTrigger.ts"
+    content = file("../../azure_functions/src/functions/caDphTimerTrigger.ts")
+  }
+
+  config_json = {}
 }
