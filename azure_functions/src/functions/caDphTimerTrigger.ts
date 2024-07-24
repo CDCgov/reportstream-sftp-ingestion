ec735c0d8a4c5fb5887d9b70f5527d31a970e358
Sylvie <sschuresko@flexion.us>
2024-07-24 12:42:23 -0500

    Update queue message contents; clean up completed/outdated TODOs
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>
    Co-Authored-By: jherrflexion <118225331+jherrflexion@users.noreply.github.com>
    Co-Authored-By: pluckyswan <96704946+pluckyswan@users.noreply.github.com>
    Co-Authored-By: Samuel Aquino <saquino@flexion.us>

--- azure_functions/src/functions/caDphTimerTrigger.ts
+++ azure_functions/src/functions/caDphTimerTrigger.ts
@@ -2,7 +2,6 @@ import { app, InvocationContext, Timer, input } from "@azure/functions";
 import { QueueServiceClient } from "@azure/storage-queue";
 
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-// TODO - add env vars to docker compose?
 const pollingTriggerQueueName = process.env.POLLING_TRIGGER_QUEUE_NAME;
 const queueServiceClient = QueueServiceClient.fromConnectionString(connectionString);
 
@@ -15,21 +14,16 @@ export async function caDphTimerTrigger(myTimer: Timer, context: InvocationConte
     console.log("Context:")
     console.log(context);
     // context.extraInputs.get("customer")
-    // TODO - send a real message
-
-    // TODO - Adjust the visibility timeout to be a fairly high value.  We want to avoid enqueuing the message and then having the
-    // polling handler kick off to download it but the download takes longer than the timeout, this would in theory kick off an infinite loop
-    // that would lock out our account
 
     // We set the visibility timeout for the message on reading, in queue.go
     // messageTimeToLive of -1 means the message does not expire
-    const sendMessageResponse = await queueClient.sendMessage("cheezburger", {messageTimeToLive: -1})
+    // the queue message contents will (in future) be the key to client-specific config
+    const sendMessageResponse = await queueClient.sendMessage("cadph", {messageTimeToLive: -1})
     console.log("Sent message successfully, service assigned message Id:", sendMessageResponse.messageId, "service assigned request Id:", sendMessageResponse.requestId );
 
     context.log('Timer function processed request.');
 }
 // TODO - set up the right CRON expression
-// TODO - figure out how we make sure there's only one Azure Function running - we should alarm if there's more than one
 // TODO - figure out if we can add multiple timers (like one per external customer?) to the same function
 // TODO - find out the timer's timezone for scheduling - there's a `schedule: { adjustForDST: true }` setting in the timer
 // TODO - Azure SDK Timeout setup research for when calling azure doesn't work.
