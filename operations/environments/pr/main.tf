e6c6b412602eb81aa93e89e9a0679880909a41ee
saquino0827 <saquino@flexion.us>
2024-08-05 15:42:19 -0500

    Update PR environment with network configurations
    Co-authored-by: jcrichlake <145698165+jcrichlake@users.noreply.github.com>
    Co-authored-by: James Herr <jherr@flexion.us>
    Co-authored-by: halprin <halprin@users.noreply.github.com>

--- operations/environments/pr/main.tf
+++ operations/environments/pr/main.tf
@@ -28,20 +28,23 @@ resource "azurerm_resource_group" "group" { //create the PR resource group becau
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "vnet" { //create the PR Vnet because it has a dynamic name that cannot be always pre-created
-  name                = "csels-rsti-pr${var.pr_number}-moderate-sftp-app-vnet"
-  location            = azurerm_resource_group.group.location
-  resource_group_name = azurerm_resource_group.group.name
-
-  address_space = ["10.0.0.0/25"]
-}
-
 module "template" {
   source = "../../template/"
 
   environment = "pr${var.pr_number}"
   deployer_id = "d59c2c86-de5e-41b7-a752-0869a73f5a60" //github app registration in Flexion Azure Entra
 
-  depends_on = [azurerm_resource_group.group, azurerm_virtual_network.vnet]
   cron       = "0 30 9 * Feb Mon"
+
+  depends_on = [
+    azurerm_resource_group.group,
+    azurerm_virtual_network.vnet,
+    azurerm_network_security_group.app_security_group,
+    azurerm_network_security_rule.App_Splunk_UF_omhsinf,
+    azurerm_network_security_rule.App_Splunk_Indexer_Discovery_omhsinf,
+    azurerm_network_security_rule.App_Safe_Encase_Monitoring_omhsinf,
+    azurerm_network_security_rule.App_ForeScout_Manager_omhsinf,
+    azurerm_network_security_rule.App_BigFix_omhsinf,
+    azurerm_network_security_rule.App_Allow_All_Out_omhsinf,
+  ]
 }
