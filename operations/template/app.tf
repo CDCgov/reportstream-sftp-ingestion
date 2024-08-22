c09071cf5f5bea0ae122c89c1106532f3b7160c2
Sylvie <sschuresko@flexion.us>
2024-08-22 09:59:54 -0500

    better name and whitespace
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>
    Co-Authored-By: Bella L. Quintero <96704946+pluckyswan@users.noreply.github.com>
    Co-Authored-By: halprin <halprin@users.noreply.github.com>
    Co-Authored-By: Samuel Aquino <saquino@flexion.us>

--- operations/template/app.tf
+++ operations/template/app.tf
@@ -12,7 +12,8 @@ resource "azurerm_container_registry" "registry" {
     ]
   }
 }
-resource "azurerm_role_assignment" "example" {
+
+resource "azurerm_role_assignment" "allow_app_to_pull_from_registry" {
   principal_id                     = azurerm_linux_web_app.sftp.identity.0.principal_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.registry.id
