69ab6f03f74ed8771f85bce817cc672658f2d336
Sylvie <sschuresko@flexion.us>
2024-09-04 11:56:31 -0500

    add a useless env var to force a deploy
    Co-Authored-By: Samuel Aquino <saquino@flexion.us>
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>

--- operations/template/app.tf
+++ operations/template/app.tf
@@ -119,6 +119,7 @@ resource "azurerm_linux_web_app" "sftp" {
     PORT                       = 8080
 
     ENV                             = var.environment
+    ENV2                            = var.environment
     AZURE_STORAGE_CONNECTION_STRING = azurerm_storage_account.storage.primary_blob_connection_string
     REPORT_STREAM_URL_PREFIX        = "https://${local.rs_domain_prefix}prime.cdc.gov"
     FLEXION_PRIVATE_KEY_NAME        = azurerm_key_vault_secret.mock_public_health_lab_private_key.name
