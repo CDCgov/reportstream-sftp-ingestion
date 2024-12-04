d47f5bc314c0b35fe4449657a6f9f3e3321cc98a
jcrichlake <jeff.crichlake@agile6.com>
2024-12-04 16:21:28 -0500

    Adding happy path tests

--- src/utils/config_test.go
+++ src/utils/config_test.go
@@ -1,31 +1,55 @@
 package utils
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func Test_populateConfig(t *testing.T) {
+func Test_populateConfig_populates(t *testing.T) {
 	jsonInput := []byte(`{
-	"displayName": "Displayed Name",
-	"isActive": true
-}`)
-	test := partnerConfig{}
-
-	test.populatePartnerConfig(jsonInput)
-
-}
-func Test_populateConfigEntry(t *testing.T) {
-	jsonInput := []byte(`{
-	"partnerId": "ca-phl",
-	"partnerSettings": {
-	"displayName": "Displayed Name",
+	"displayName": "Test Name",
 	"isActive": true,
 	"sftpConnectionType": "external",
 	"hasZipPassword": true,
 	"defaultEncoding": "ISO-8859-1",
 	"cronExpression": "* * * * *",
-	"containerName": "blah"
+	"containerName": "container-name"
+}`)
+	test := partnerConfig{}
+
+	output := test.populatePartnerConfig(jsonInput)
+
+	assert.Contains(t, output.DisplayName, "Test Name")
+	assert.Equal(t, output.IsActive, true)
+	assert.Contains(t, output.SftpConnectionType, "external")
+	assert.Equal(t, output.HasZipPassword, true)
+	assert.Contains(t, output.DefaultEncoding, "ISO-8859-1")
+	assert.Contains(t, output.CronExpression, "* * * * *")
+	assert.Contains(t, output.ContainerName, "container-name")
+
+}
+func Test_populateConfigEntry_populates(t *testing.T) {
+	jsonInput := []byte(`{
+	"partnerId": "ca-phl",
+	"partnerSettings": {
+	"displayName": "Test Name",
+	"isActive": true,
+	"sftpConnectionType": "external",
+	"hasZipPassword": true,
+	"defaultEncoding": "ISO-8859-1",
+	"cronExpression": "* * * * *",
+	"containerName": "container-name"
 }}`)
 	test := partnerConfig{}
 
-	_, _ = test.populateConfigEntry(jsonInput)
+	output, _ := test.populateConfigEntry(jsonInput)
 
+	assert.Contains(t, output.PartnerId, "ca-phl")
+	assert.Contains(t, output.PartnerConfig.DisplayName, "Test Name")
+	assert.Equal(t, output.PartnerConfig.IsActive, true)
+	assert.Contains(t, output.PartnerConfig.SftpConnectionType, "external")
+	assert.Equal(t, output.PartnerConfig.HasZipPassword, true)
+	assert.Contains(t, output.PartnerConfig.DefaultEncoding, "ISO-8859-1")
+	assert.Contains(t, output.PartnerConfig.CronExpression, "* * * * *")
+	assert.Contains(t, output.PartnerConfig.ContainerName, "container-name")
 }
