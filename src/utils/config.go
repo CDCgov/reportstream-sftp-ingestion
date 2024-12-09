0ae56ecafbb66c86c64aced1437471b5fcf828d4
jcrichlake <145698165+jcrichlake@users.noreply.github.com>
2024-12-09 11:18:19 -0500

    Config client (#244)
    ---------
    
    Co-authored-by: Sylvie <sschuresko@flexion.us>
    Co-authored-by: halprin <halprin@users.noreply.github.com>

---
+++ src/utils/config.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"slices"
+	"time"
+)
+
+var allowedEncodingList = []string{"ISO-8859-1", "UTF-8"}
+
+//https://www.notion.so/flexion-cdc-ti/Thinking-about-config-eb9424dafea14320be5cee1b8b03d890?pvs=4
+
+// All the details each partner should have
+// to null-equivalent, but we'll want to check for valid values
+type PartnerSettings struct {
+	DisplayName              string `json:"displayName"` // full name if we need pretty names
+	IsActive                 bool   `json:"isActive"`
+	IsExternalSftpConnection bool   `json:"isExternalSftpConnection"`
+	HasZipPassword           bool   `json:"hasZipPassword"`
+	DefaultEncoding          string `json:"defaultEncoding"`
+}
+
+// When did we last get the file and what did we parse out of it?
+type Config struct {
+	lastRetrieved   time.Time
+	partnerSettings PartnerSettings
+	PartnerId       string //unique name, put in queue message from polling function - keep this short so we can use it in TF resources? Currently TF and RS use `ca-phl`
+}
+
+func (config *Config) populatePartnerSettings(input []byte) error {
+
+	var partnerSettings PartnerSettings
+	err := json.Unmarshal(input, &partnerSettings)
+
+	if err != nil {
+		slog.Error("Unable unmarshall to partner settings", slog.Any(ErrorKey, err))
+		return err
+	}
+
+	err = validateDefaultEncoding(partnerSettings.DefaultEncoding)
+	if err != nil {
+		slog.Error("Invalid encoding found", slog.Any(ErrorKey, err), slog.String("Partner ID", config.PartnerId), slog.String("Encoding", partnerSettings.DefaultEncoding))
+		return err
+	}
+
+	config.partnerSettings = partnerSettings
+	return nil
+
+}
+
+func validateDefaultEncoding(input string) error {
+	if slices.Contains(allowedEncodingList, input) {
+		return nil
+	}
+	return errors.New("Invalid encoding found: " + input)
+}
---
+++ src/utils/config_test.go
@@ -0,0 +1,62 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"log/slog"
+	"testing"
+)
+
+func Test_populatePartnerSettings_populates(t *testing.T) {
+	jsonInput := []byte(`{
+	"displayName": "Test Name",
+	"isActive": true,
+	"isExternalSftpConnection": true,
+	"hasZipPassword": true,
+	"defaultEncoding": "ISO-8859-1"
+}`)
+	test := Config{}
+
+	_ = test.populatePartnerSettings(jsonInput)
+
+	assert.Contains(t, test.partnerSettings.DisplayName, "Test Name")
+	assert.Equal(t, test.partnerSettings.IsActive, true)
+	assert.Equal(t, test.partnerSettings.IsExternalSftpConnection, true)
+	assert.Equal(t, test.partnerSettings.HasZipPassword, true)
+}
+
+func Test_populatePartnerSettings_errors_whenJsonInvalid(t *testing.T) {
+	jsonInput := []byte(`bad json`)
+	test := Config{}
+
+	err := test.populatePartnerSettings(jsonInput)
+
+	assert.Error(t, err)
+
+}
+
+func Test_populatePartnerSettings_errors_whenEncodingInvalid(t *testing.T) {
+	jsonInput := []byte(`{
+	"displayName": "Test Name",
+	"isActive": true,
+	"isExternalSftpConnection": true,
+	"hasZipPassword": true,
+	"defaultEncoding": "Something else"
+}`)
+
+	buffer, defaultLogger := SetupLogger()
+	defer slog.SetDefault(defaultLogger)
+
+	test := Config{}
+
+	err := test.populatePartnerSettings(jsonInput)
+
+	assert.Error(t, err)
+	assert.Contains(t, buffer.String(), "Invalid encoding found")
+}
+
+func Test_validateDefaultEncoding_errors(t *testing.T) {
+
+	err := validateDefaultEncoding("bad encoding")
+
+	assert.Error(t, err)
+}
