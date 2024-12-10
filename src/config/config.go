e705a5dcd87ac608c1689fd00c1debe62a85303a
Sylvie <sschuresko@flexion.us>
2024-12-10 13:53:21 -0600

    reorganize TODO based on when we should TODONE
    Co-Authored-By: jcrichlake <145698165+jcrichlake@users.noreply.github.com>

--- src/config/config.go
+++ src/config/config.go
@@ -32,9 +32,15 @@ var Configs map[string]*Config
 
 /*
 TODO list as of Dec 10:
+Current PR:
 - Add tests for NewConfig?
-- Set up another function trigger/CRON for Flexion
 - Set up config files for ca-phl and flexion in all envs (need to change what's in local too - it currently only has one value)
+	- Do we want to add some kind of logging indicating what config got loaded? Maybe just temporarily for testing purposes?
+- ADR for not-crashing if one config fails
+- ADR for config in general
+
+Future PR:
+- Set up another function trigger/CRON for Flexion
 - What happens if you try to retrieve a map index that doesn't exist? Need to check for errors or nils or something everywhere we get config
 - In polling message handler, use queue message to:
 	- decide whether to do retrieval ('no' for flexion probs)
@@ -48,9 +54,6 @@ TODO list as of Dec 10:
 - See if we need to do add'l TF to set up Flexion?
 	- probably at least a cron expression and RS config. It would be nice to have an external Flexion SFTP site to hit for testing
 	- Do we want to start making TF dynamic at this point or wait for add'l partners? I think maybe wait for 1-2 more partners?
-- ADR for not-crashing if one config fails
-- ADR for config in general
-
 */
 
 func NewConfig(partnerId string) (*Config, error) {
